1eb24b57d01e33fff47931110262c7a467d72db5
pqguanyinli <34409013+pqguanyinli@users.noreply.github.com>
2019-07-28 22:47:05 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,6 +3,9 @@ if [ ! -f /conf/config.php ]; then
 	cp /guanfreenom/config.php /conf/config.php
 fi
 
+if [ -f /freenom/config.php ]; then
+        rm -rf /freenom/config.php && \
+fi
 ln -s /conf/config.php /freenom/config.php
 cd /freenom/
 php index.php
